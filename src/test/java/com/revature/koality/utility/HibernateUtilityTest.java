92c64f1783267118724e9f3a6e9b16d244df0515
Eddy Soma <esoma92.dev@gmail.com>
2019-01-04 03:24:45 -0500

    hibernate session factory setup and test

---
+++ src/test/java/com/revature/koality/utility/HibernateUtilityTest.java
@@ -0,0 +1,32 @@
+package com.revature.koality.utility;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HibernateUtilityTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		HibernateUtility.getMainSessionFactory();
+		HibernateUtility.getTestSessionFactory();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		HibernateUtility.destroySessionFactories();
+	}
+
+	@Test
+	public void testGetMainSessionFactory() {
+		assertNotNull(HibernateUtility.getMainSessionFactory());
+	}
+
+	@Test
+	public void testGetTestSessionFactory() {
+		assertNotNull(HibernateUtility.getTestSessionFactory());
+	}
+
+}
