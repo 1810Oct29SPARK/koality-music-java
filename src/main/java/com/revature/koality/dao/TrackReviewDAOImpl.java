7b05329c1ff2c2172928c14a85a74f023e742795
Eddy Soma <esoma92.dev@gmail.com>
2019-01-06 15:43:49 -0500

    implement track review dao

---
+++ src/main/java/com/revature/koality/dao/TrackReviewDAOImpl.java
@@ -0,0 +1,144 @@
+package com.revature.koality.dao;
+
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.revature.koality.bean.Customer;
+import com.revature.koality.bean.ReviewContent;
+import com.revature.koality.bean.Track;
+import com.revature.koality.bean.TrackReview;
+import com.revature.koality.utility.HibernateUtility;
+
+public class TrackReviewDAOImpl implements TrackReviewDAO {
+
+	private SessionFactory sessionFactory;
+
+	public TrackReviewDAOImpl() {
+		this.sessionFactory = HibernateUtility.getMainSessionFactory();
+	}
+
+	public TrackReviewDAOImpl(boolean isTest) {
+		if (isTest) {
+			this.sessionFactory = HibernateUtility.getTestSessionFactory();
+		} else {
+			this.sessionFactory = HibernateUtility.getMainSessionFactory();
+		}
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	public int addTrackReview(ReviewContent reviewContent, int trackId, int customerId) {
+
+		int id = -1;
+		Session session = null;
+
+		if (this.sessionFactory != null) {
+			try {
+				session = this.sessionFactory.getCurrentSession();
+				session.beginTransaction();
+				TrackReview trackReview = new TrackReview(reviewContent);
+				trackReview.setTrack(session.load(Track.class, trackId));
+				trackReview.setCustomer(session.load(Customer.class, customerId));
+				id = (int) session.save(trackReview);
+				session.getTransaction().commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+				session.getTransaction().rollback();
+				id = -1;
+			} finally {
+				session.close();
+			}
+		}
+
+		return id;
+
+	}
+
+	@Override
+	public TrackReview getTrackReviewById(int trackReviewId) {
+
+		TrackReview trackReview = null;
+		Session session = null;
+
+		if (this.sessionFactory != null) {
+			try {
+				session = this.sessionFactory.getCurrentSession();
+				session.beginTransaction();
+				trackReview = session.get(TrackReview.class, trackReviewId);
+				Hibernate.initialize(trackReview.getTrack());
+				Hibernate.initialize(trackReview.getCustomer());
+				session.getTransaction().commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+				session.getTransaction().rollback();
+			} finally {
+				session.close();
+			}
+		}
+
+		return trackReview;
+
+	}
+
+	@Override
+	public TrackReview getTrackReviewByTrackIdAndCustomerId(int trackId, int customerId) {
+
+		TrackReview trackReview = null;
+		Session session = null;
+
+		if (this.sessionFactory != null) {
+			try {
+				String hql = "FROM TrackReview tr WHERE tr.track.trackId = :trackId AND tr.customer.customerId = :customerId";
+				session = this.sessionFactory.getCurrentSession();
+				session.beginTransaction();
+				trackReview = session.createQuery(hql, TrackReview.class).setParameter("trackId", trackId)
+						.setParameter("customerId", customerId).getSingleResult();
+				Hibernate.initialize(trackReview.getTrack());
+				Hibernate.initialize(trackReview.getCustomer());
+				session.getTransaction().commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+				session.getTransaction().rollback();
+			} finally {
+				session.close();
+			}
+		}
+
+		return trackReview;
+
+	}
+
+	@Override
+	public boolean deleteTrackReview(int trackReviewId) {
+
+		Session session = null;
+
+		if (this.sessionFactory != null) {
+			try {
+				session = this.sessionFactory.getCurrentSession();
+				session.beginTransaction();
+				TrackReview trackReview = session.load(TrackReview.class, trackReviewId);
+				session.delete(trackReview);
+				session.getTransaction().commit();
+				return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+				session.getTransaction().rollback();
+			} finally {
+				session.close();
+			}
+		}
+
+		return false;
+
+	}
+
+}
---
+++ src/test/java/com/revature/koality/dao/TrackReviewDAOImplTest.java
@@ -0,0 +1,77 @@
+package com.revature.koality.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.revature.koality.utility.CommonUtility;
+import com.revature.koality.utility.HibernateUtility;
+import com.revature.koality.utility.MockUtility;
+
+public class TrackReviewDAOImplTest {
+
+	private static TrackReviewDAO trd;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		trd = new TrackReviewDAOImpl(true);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		HibernateUtility.destroySessionFactories();
+	}
+
+	@Ignore
+	@Test
+	public void testAddTrackReview() {
+		assertNotEquals(-1, trd.addTrackReview(MockUtility.getMockReviewContent(),
+				CommonUtility.generateRandomInteger(13), CommonUtility.generateRandomInteger(6)));
+	}
+
+	@Ignore
+	@Test
+	public void testAddTrackReview2() {
+		assertNotEquals(-1, trd.addTrackReview(MockUtility.getMockReviewContent(),
+				CommonUtility.generateRandomInteger(13), CommonUtility.generateRandomInteger(6)));
+	}
+
+	@Ignore
+	@Test
+	public void testAddTrackReview3() {
+		assertNotEquals(-1, trd.addTrackReview(MockUtility.getMockReviewContent(),
+				CommonUtility.generateRandomInteger(13), CommonUtility.generateRandomInteger(6)));
+	}
+
+	@Test
+	public void testGetTrackReviewById() {
+		assertEquals(10, trd.getTrackReviewById(2).getReviewContent().getRating());
+	}
+
+	@Test
+	public void testGetTrackReviewByIdWithTrack() {
+		assertEquals(375, trd.getTrackReviewById(1).getTrack().getTrackLength());
+	}
+
+	@Test
+	public void testGetTrackReviewByTrackIdAndCustomerId() {
+		assertEquals("cBrSMmi9HY7C03W5GwM3si98lfZVrSTsQ5E325qtZC4sU2Gejq",
+				trd.getTrackReviewByTrackIdAndCustomerId(3, 4).getReviewContent().getReviewComment());
+	}
+
+	@Test
+	public void testGetTrackReviewByTrackIdAndCustomerIdWithCustomer() {
+		assertEquals("WmQmUg",
+				trd.getTrackReviewByTrackIdAndCustomerId(1, 5).getCustomer().getCustomerDetail().getFirstName());
+	}
+
+	@Ignore
+	@Test
+	public void testDeleteTrackReview() {
+		assertTrue(trd.deleteTrackReview(7));
+	}
+
+}
