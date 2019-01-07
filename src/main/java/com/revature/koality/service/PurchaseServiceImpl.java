e3c223c59f468239489d638acd4f6b88d00d8202
dannytrev21 <danny.trev21@gmail.com>
2019-01-07 16:19:34 -0500

    A service for purchasing tracks and albums

---
+++ src/main/java/com/revature/koality/service/PurchaseServiceImpl.java
@@ -0,0 +1,27 @@
+package com.revature.koality.service;
+
+import com.revature.koality.dao.CustomerDAO;
+import com.revature.koality.dao.CustomerDAOImpl;
+
+public class PurchaseServiceImpl implements PurchaseService {
+	
+	private CustomerDAO cd;
+
+	public PurchaseServiceImpl() {
+		super();
+		cd = new CustomerDAOImpl(); 
+	}
+
+	@Override
+	public boolean purchaseTrack(int customerId, int trackId) {
+		
+		return cd.purchaseTrack(customerId, trackId); 
+	}
+
+	@Override
+	public boolean purchaseAlbum(int customerId, int albumId) {
+		
+		return cd.purchaseAlbum(customerId, albumId); 
+	}
+
+}
