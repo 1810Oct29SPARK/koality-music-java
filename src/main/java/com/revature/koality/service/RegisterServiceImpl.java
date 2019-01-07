55f3ad819f1aebf2fdf16fa0d185416f71d01d08
dannytrev21 <danny.trev21@gmail.com>
2019-01-06 20:27:02 -0500

    authentication and register services

---
+++ src/main/java/com/revature/koality/service/RegisterServiceImpl.java
@@ -0,0 +1,65 @@
+package com.revature.koality.service;
+
+import com.revature.koality.bean.Customer;
+import com.revature.koality.bean.CustomerCredentials;
+import com.revature.koality.bean.CustomerDetail;
+import com.revature.koality.bean.Image;
+import com.revature.koality.bean.Publisher;
+import com.revature.koality.bean.PublisherCredentials;
+import com.revature.koality.bean.PublisherDetail;
+import com.revature.koality.dao.CustomerDAO;
+import com.revature.koality.dao.CustomerDAOImpl;
+import com.revature.koality.dao.PublisherDAO;
+import com.revature.koality.dao.PublisherDAOImpl;
+import com.revature.koality.utility.CommonUtility;
+
+public class RegisterServiceImpl implements RegisterService {
+
+	public RegisterServiceImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int RegisterCustomer(String firstName, String lastName, String email, String favoriteGenre, String username,
+			String password) {
+		
+		CustomerDAO cd = new CustomerDAOImpl();
+
+		CustomerDetail customerDetail = new CustomerDetail(firstName, lastName, email, favoriteGenre);
+		String hashSalt = CommonUtility.generateRandomString(4);
+
+		String createHash = password + hashSalt;
+
+		String passwordHash = CommonUtility.digestSHA256(createHash);
+		
+		CustomerCredentials customerCredentials = new CustomerCredentials(username,hashSalt,passwordHash); 
+		
+		Image image = null; 
+		
+		Customer customer = new Customer(customerDetail, image , customerCredentials); 
+		
+		return cd.addCustomer(customer); 
+	}
+
+	@Override
+	public int RegisterPublisher(String firstName, String lastName, String email, String companyName, String username,
+			String password) {
+		PublisherDAO pd = new PublisherDAOImpl();
+
+		PublisherDetail publisherDetail = new PublisherDetail(firstName, lastName, email, companyName);
+		String hashSalt = CommonUtility.generateRandomString(4);
+
+		String createHash = password + hashSalt;
+
+		String passwordHash = CommonUtility.digestSHA256(createHash);
+
+		PublisherCredentials publisherCredentials = new PublisherCredentials(username, hashSalt, passwordHash);
+
+		Image image = null;
+
+		Publisher publisher = new Publisher(publisherDetail, image, publisherCredentials);
+
+		return pd.addPublisher(publisher);
+	}
+
+}
---
+++ src/test/java/com/revature/koality/service/AuthenticationServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.revature.koality.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AuthenticationServiceImplTest {
+	
+	AuthenticationService as = new AuthenticationServiceImpl(true); 
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testIsValidPublisher() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsValidCustomer() {
+		fail("Not yet implemented");
+	}
+
+}
