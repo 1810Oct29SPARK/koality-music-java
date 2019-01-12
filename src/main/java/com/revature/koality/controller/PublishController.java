f2703e6b2c9836dc645eb553fb7d3d8a3d7084b1
Eddy Soma <esoma92.dev@gmail.com>
2019-01-12 11:05:58 -0500

    implement dry controller for store and publish

---
+++ src/main/java/com/revature/koality/controller/PublishController.java
@@ -0,0 +1,119 @@
+package com.revature.koality.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.koality.utility.CommonUtility;
+
+@RestController
+public class PublishController {
+
+	// SERVICE DECLARATION
+
+	public PublishController() {
+		super();
+	}
+
+	// SERVICE SETTER
+
+	@PostMapping("/publish-track")
+	public void publishTrack(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		int status = 418;
+		Integer id = -1;
+
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			try (BufferedReader br = request.getReader()) {
+				int publisherId = Integer.parseInt(session.getAttribute("publisherId").toString());
+
+				String requestBody = CommonUtility.readRequest(br);
+				JSONObject jo = new JSONObject(requestBody);
+
+				String trackName = jo.getString("trackName");
+				String genre = jo.getString("genre");
+				String composer = jo.getString("composer");
+				String artist = jo.getString("artist");
+				int trackLength = jo.getInt("trackLength");
+				float unitPrice = jo.getFloat("unitPrice");
+				String audioType = jo.getString("audioType");
+				byte[] audioData = CommonUtility.decodeBlobUrl(jo.getString("audioData"));
+
+				// SERVICE
+				if (id != -1) {
+					status = 200;
+				} else {
+					status = 400;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				status = 400;
+			}
+		} else {
+			status = 440;
+		}
+
+		response.setStatus(status);
+		response.getWriter().write(id.toString());
+
+	}
+
+	@PostMapping("/publish-album")
+	public void publishAlbum(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		int status = 418;
+		Integer id = -1;
+
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			try (BufferedReader br = request.getReader()) {
+				int publisherId = Integer.parseInt(session.getAttribute("publisherId").toString());
+
+				String requestBody = CommonUtility.readRequest(br);
+				JSONObject jo = new JSONObject(requestBody);
+
+				String albumName = jo.getString("albumName");
+				String genre = jo.getString("genre");
+				float unitPrice = jo.getFloat("unitPrice");
+				String imageType = jo.getString("imageType");
+				byte[] imageData = CommonUtility.decodeBlobUrl(jo.getString("imageData"));
+
+				List<Integer> trackIdList = new ArrayList<>();
+				JSONArray trackIdArray = jo.getJSONArray("trackIdList");
+				Iterator<Object> iter = trackIdArray.iterator();
+				while (iter.hasNext()) {
+					trackIdList.add(Integer.parseInt(iter.next().toString()));
+				}
+
+				// SERVICE
+				if (id != -1) {
+					status = 200;
+				} else {
+					status = 400;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				status = 400;
+			}
+		} else {
+			status = 440;
+		}
+
+		response.setStatus(status);
+		response.getWriter().write(id.toString());
+
+	}
+
+}
